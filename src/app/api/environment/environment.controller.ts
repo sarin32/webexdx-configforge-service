ea52fb1f952b88d6a63c7c36ea0ad6e7457b75f2
Sarin Alexander <execode02@gmail.com>
2025-05-10 16:37:17 +0530

    [FIX] import fix

--- src/app/api/environment/environment.controller.ts
+++ src/app/api/environment/environment.controller.ts
@@ -2,19 +2,10 @@ import { Context } from 'koa';
 import { BadRequestError, ForbiddenError } from '../../errors';
 import { environmentService } from '../../services/environment/environment.service';
 import {
-  objectIdSchema,
-  objectSchema,
-  stringSchema,
   validateObject,
 } from '../../utils/schema-validator';
 import { objectId } from '../../utils/data-type-util';
-
-const createEnvironmentSchema = objectSchema({
-  object: {
-    name: stringSchema({ min: 1 }),
-    projectId: objectIdSchema(),
-  },
-});
+import { createEnvironmentSchema } from './environment.schema';
 
 export async function createEnvironment(ctx: Context) {
   const { error, value } = validateObject<{
