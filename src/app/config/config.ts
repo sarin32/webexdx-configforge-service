afdfe6774f7759ffe91f65869361b004cce02098
Sarin Alexander <execode02@gmail.com>
2026-05-06 23:49:46 +0530

    feat: koa-wrap update

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -9,7 +9,13 @@ const env = process.env;
 
 export const PORT = Number(env.PORT!);
 
-export const NODE_ENV = env.NODE_ENV;
+export enum NodeEnv {
+  DEVELOPMENT = 'development',
+  PRODUCTION = 'production',
+  TEST = 'test',
+}
+
+export const NODE_ENV = (env.NODE_ENV as NodeEnv) || NodeEnv.DEVELOPMENT;
 
 export const DATABASE_SETTINGS = {
   URL: env.DATABASE_URL!,
@@ -17,3 +23,14 @@ export const DATABASE_SETTINGS = {
 
 // Token validation settings (needed for auth service integration)
 export const SECRET_TOKEN = env.JWT_SECRET_TOKEN!;
+
+export const LOG_SETTINGS = {
+  PRINT: {
+    COLORIZE: NODE_ENV === NodeEnv.DEVELOPMENT,
+    LEVEL: env.PRINT_LOG_LEVEL || 'debug',
+  },
+  FILE: {
+    LOG_TO_FILE: NODE_ENV === NodeEnv.DEVELOPMENT,
+    LEVEL: env.FILE_LOG_LEVEL || 'debug',
+  },
+};
